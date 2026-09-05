7784ad3e71c5e842bbb4101807b0dfb5efb9f3eb
Shahab Hamidi-Rad <shahab.hamidi-rad@interdigital.com>
2026-09-04 23:13:38 -0700

    Adding support for pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-import sys
 import setuptools
 
 # Get version from "neoradium/__init__.py":
@@ -21,12 +20,19 @@ setuptools.setup(name="neoradium",
                  author_email = "shahab.hamidi-rad@interdigital.com",
                  description = "NeoRadium 3GPP 5G NR wireless communication python library",
                  long_description = open("README.md", "r", encoding="utf-8").read(),
-                 license = open("LICENSE", "r", encoding="utf-8").read(),
-                 url = "http://www.interdigital.com",
-                 packages = ['neoradium'],
+                 long_description_content_type = 'text/markdown',
+                 license = 'InterDigital Limited Software Evaluation License',
+                 url = 'https://github.com/InterDigitalInc/NeoRadium',
+                 project_urls = {
+                     'Source':        'https://github.com/InterDigitalInc/NeoRadium',
+                     'Documentation': 'https://interdigitalinc.github.io/NeoRadium/',
+                     'Bug Tracker':   'https://github.com/InterDigitalInc/NeoRadium/issues',
+                 },
+                 packages = ['neoradium', 'neoradium.nrext'],
                  classifiers=[ 'Development Status :: 5 - Production/Stable',
-                               'Intended Audience :: Researchers',
-                               'Topic :: Software Development',
+                               'Intended Audience :: Science/Research',
+                               'Topic :: Scientific/Engineering :: Information Analysis',
+                               'License :: Other/Proprietary License',
                                'Programming Language :: Python :: 3.10',
                                'Programming Language :: Python :: 3.11',
                                'Programming Language :: Python :: 3.12'],
